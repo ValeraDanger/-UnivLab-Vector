3cc0c84a6620fc92df587dcd7f1f83d67ff99925
ValeraDanger <47420979+ValeraDanger@users.noreply.github.com>
2023-05-01 00:53:06 +0300

    Added operators and push methods

--- -UnivLab-Vector/Vector.h
+++ -UnivLab-Vector/Vector.h
@@ -12,6 +12,9 @@ private:
 public:
 	Vector(size_t vector_dim) : dim(vector_dim) {
 		this->array = new T[dim];
+		if (this->array == nullptr) {
+			throw std::bad_alloc();
+		}
 	}
 
 	Vector(T* other, size_t vector_dim) : Vector(vector_dim) {
@@ -20,10 +23,13 @@ public:
 		}
 	}
 
-	//Vector() : this-vector_dim(0) {}
 
 	Vector(const Vector<T>& other) : dim(other.dim) {
 		this->array = new T[this->dim];
+		if (this->array == nullptr) {
+			throw std::bad_alloc();
+		}
+
 		for (size_t i = 0; i < this->dim; i++) {
 			this->array[i] = other.array[i];
 		}
@@ -97,5 +103,108 @@ public:
 		}
 
 		return res;
+	}
+
+	bool operator== (const Vector<T>& other) {
+		if (this->dim != other.dim) return false;
+
+		for (size_t i = 0; i < this->dim; i++) {
+			if ((*this)[i] != other[i]) return false;
+		}
+
+		return true;
+	}
+
+	bool operator!= (const Vector<T>& other) {
+		return !((*this) == other);
+	}
+
+	Vector& operator+= (const Vector<T>& other) {
+		if (this->dim != other.dim) {
+			throw std::range_error("Vectors sizes not equal");
+		}
+
+		for (size_t i = 0; i < this->dim; i++) {
+			(*this)[i] += other[i];
+		}
+
+		return *this;
+	}
+
+	Vector& operator+= (T num) {
+		for (size_t i = 0; i < this->dim; i++) {
+			(*this)[i] += num;
+		}
+
+		return *this;
+	}
+
+	Vector& operator++ () {		/*++Vector*/
+		(*this) += 1;
+		return *this;
+	}
+
+	Vector operator++ (int) {		/*Vector++*/
+		Vector temp = *this;
+		++(*this);
+		return temp;
+	}
+
+
+	Vector& operator-= (const Vector<T>& other) {
+		if (this->dim != other.dim) {
+			throw std::range_error("Vectors sizes not equal");
+		}
+
+		for (size_t i = 0; i < this->dim; i++) {
+			(*this)[i] -= other[i];
+		}
+
+		return *this;
+	}
+
+	Vector& operator-= (T num) {
+		for (size_t i = 0; i < this->dim; i++) {
+			(*this)[i] -= num;
+		}
+
+		return *this;
+	}
+
+	Vector& operator-- () {		/*++Vector*/
+		(*this) -= 1;
+		return *this;
+	}
+
+	Vector operator-- (int) {		/*Vector++*/
+		Vector temp = *this;
+		--(*this);
+		return temp;
+	}
+ 
+	void push_back(T elem) {
+		T* resized_array = new T[this->dim + 1];
+		if (resized_array == nullptr) {
+			throw std::bad_alloc();
+		}
+
+		memcpy(resized_array, this->array, this->dim * sizeof(T));
+		resized_array[this->dim] = elem;
+		delete[] this->array;
+		this->array = resized_array;
+		this->dim++;
+	}
+
+	void push_front(T elem) {
+		T* resized_array = new T[this->dim + 1];
+		if (resized_array == nullptr) {
+			throw std::bad_alloc();
+		}
+
+		resized_array[0] = elem;
+		memcpy(resized_array+1, this->array, this->dim * sizeof(T));
+		delete[] this->array;
+		this->array = resized_array;
+		this->dim++;
 	}
 };
